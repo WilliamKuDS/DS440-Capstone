3986a7cfd398db242d367dd3a1839797d33fd8c1
WilliamKuDS <williamkucds@gmail.com>
2022-03-01 17:05:41 -0500

    Change grayscale resolution
    Changed the grayscale images from 512 to 256. Added the new images and adjusted the values in the grayscale.py script

--- src/utilities/grayscale.py
+++ src/utilities/grayscale.py
@@ -8,6 +8,6 @@ grayscale_path = r"/Users/william/Downloads/initial/grayscale/"
 for filename in glob.glob(os.path.join(color_path, '*.png')):
     print(filename)
     img=cv2.imread(filename) 
-    rl=cv2.resize(img, (512,512))
+    rl=cv2.resize(img, (256,256))
     gray_image = cv2.cvtColor(rl, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(os.path.join(grayscale_path,filename), gray_image)
